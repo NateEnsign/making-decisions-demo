a25edfa1ffebcbfe9d5356309cba5c7fef68662a
Nate Ensign <dixienate@gmail.com>
2022-09-28 15:43:38 -0600

    final battle plays out

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -35,3 +35,36 @@ if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
     jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
     console.log (`Jon Snow's Health is down to ${jonSnowHealth}`)
 }
+
+if (jonSnowHealth + 50 >=100) {
+    jonSnowHealth = 100
+} else {
+    jonSnowHealth += 50
+}
+
+
+console.log (`Jon Snow health after the health kit is ` + jonSnowHealth)
+
+let coinLandsheads = true
+if (coinLandsHeads) {
+    console.log("The fight continues")
+} else {
+    console.log("Jon is allowed to run away")
+}
+
+//Jamie attacks Jon 5 times
+for (let i = 0; i < 5; i++) {
+    jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
+    console.log('jon\'s health is now ' + jonSnowHealth)
+}
+
+
+//Write logic in the for loop that detects if Jon's health gets 0 or less, in which case, you will print that Jon is deal
+// and you will stop futher loop iterations (see the "break" JS keyword for leaving a loop early)
+
+// Post lab assignement 2:
+//Write the for loop (including the logic your wrote for the first assignement) as a while loop.
+// It should behave exactly the same
+
+
+
